e95839fa2a42fe7a1971c3bb4e91315e7ee89048
Kiki910 <119072403+Kiki910@users.noreply.github.com>
2023-08-26 13:35:23 +0200

    Modifiche
    Introdotto la parte di MongoDB e Java

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 window.addEventListener('scroll', function() {
     var button = document.getElementById('back-to-top');
-    if (window.pageYOffset > 400) {
+    var bodyAlt = document.body.offsetHeight;
+    var scrollPos = window.innerHeight + window.pageYOffset;
+
+    if (window.pageYOffset > 400 && scrollPos < (bodyAlt - 150)) {
         button.style.display = 'block';
     } else {
         button.style.display = 'none';
